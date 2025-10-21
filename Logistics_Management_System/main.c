f2840cda12c840acbb3c8a0cb675aa8d353d8d04
ishini672 <as20240591@sci.sjp.ac.lk>
2025-10-21 15:20:55 +0530

    add variables for vehicle management

--- Logistics_Management_System/main.c
+++ Logistics_Management_System/main.c
@@ -9,6 +9,15 @@ char cityNames[MAX_CITIES][30];
 int distance[MAX_CITIES][MAX_CITIES];
 int cityCount = 0;
 
+char vehicleNames[3][10] = {"Van", "Truck", "Lorry"};
+double ratePerKm[3] = {30, 40, 80};
+double avgSpeed[3] = {60, 50, 45};
+double fuelEff[3] = {12, 6, 4};
+double fuelPrice = 310; // LKR per liter
+
+
+
+
 
 
 
