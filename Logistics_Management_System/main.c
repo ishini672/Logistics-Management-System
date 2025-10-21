90fb091faf1ca8735bbcdb5d7bdd3dc5dd9f0fd3
ishini672 <as20240591@sci.sjp.ac.lk>
2025-10-21 15:26:27 +0530

    add variables for calculations part

--- Logistics_Management_System/main.c
+++ Logistics_Management_System/main.c
@@ -15,6 +15,18 @@ double avgSpeed[3] = {60, 50, 45};
 double fuelEff[3] = {12, 6, 4};
 double fuelPrice = 310; // LKR per liter
 
+int fromCity[MAX_DELIVERIES];
+int toCity[MAX_DELIVERIES];
+int vehicleType[MAX_DELIVERIES];
+int dist[MAX_DELIVERIES];
+double weight[MAX_DELIVERIES];
+double baseCost[MAX_DELIVERIES], fuelUsed[MAX_DELIVERIES];
+double fuelCost[MAX_DELIVERIES], totalCost[MAX_DELIVERIES];
+double profit[MAX_DELIVERIES], customerCharge[MAX_DELIVERIES];
+double timeHrs[MAX_DELIVERIES];
+int deliveryCount = 0;
+
+
 
 
 
